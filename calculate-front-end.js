66a1ffe1bab4bf907b057cd964bb4b7b9dd0c64a
Carlos L Molina <carlos.molina@codeimmersives.com>
2020-06-23 14:59:17 -0400

    changes

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,7 +1,14 @@
 /*********************************************
  * YOUR CODE HERE!                           *
  ********************************************/
+module.exports= require-('.calculate-back-end')
 
+
+userInput= getInput(0)
+userInput2= getInput(1)
+
+
+getInput(3);
 
 
 
